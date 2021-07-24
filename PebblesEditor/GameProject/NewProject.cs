cfb452cc5585a361373b273198cca79c557dfc31
QuLink1040 <44401758+QuLink1040@users.noreply.github.com>
2021-07-24 18:38:38 +0800

    Update with partially implemented template loading 28:02

--- PebblesEditor/GameProject/NewProject.cs
+++ PebblesEditor/GameProject/NewProject.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
 
 namespace PebblesEditor.GameProject
 {
+    [DataContract]
+
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+    }
+    
     class NewProject : ViewModelBase
     {
+        //TODO: get path from install location
+        private readonly string _templatePath = @"..\..\PebblesEditor\Resources\Project Templates";
+
         private string _name = "NewProject";
         public string Name
         {
@@ -31,6 +51,31 @@ namespace PebblesEditor.GameProject
                     _name = value;
                     OnPropertyChange(nameof(Path));
                 }
+            }
+        }
+
+        public NewProject()
+        {
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = new ProjectTemplate()
+                    {
+                        ProjectType = "Empty Project",
+                        ProjectFile = "project.pebbles", 
+                        Folders = new List<string> () {".Pebbles", "Content", "GameCode" }
+                    };
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: log error
             }
         }
     }
