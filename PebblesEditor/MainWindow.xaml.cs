be20931f53b8a7156d812938ad28400d2f9a0b49
Calen Tang <44401758+QuLink1040@users.noreply.github.com>
2021-07-26 00:21:19 +0800

    Update with title functionality implemented; app icon not showing

--- PebblesEditor/MainWindow.xaml.cs
+++ PebblesEditor/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +26,10 @@ namespace PebblesEditor
         public MainWindow()
         {
             InitializeComponent();
+            this.Height = SystemParameters.WorkArea.Height;
+            this.Width = SystemParameters.WorkArea.Width;
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+            StateChanged += MainWindowStateChangeRaised;
             Loaded += OnMainWindowLoaded;
             Closing += OnMainWindowClosing;
         }
@@ -52,7 +57,70 @@ namespace PebblesEditor
             {
                 Project.Current?.Unload();
                 DataContext = projectBrowser.DataContext;
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
             }
         }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MainWindowStateChangeRaised(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                this.BorderThickness = new Thickness(6);
+            }
+            else
+            {
+                this.BorderThickness = new Thickness(0);
+            }
+        }
+
+        private void Window_RightMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Right)
+            {
+                IntPtr hWnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+                RECT pos;
+                GetWindowRect(hWnd, out pos);
+                IntPtr hMenu = GetSystemMenu(hWnd, false);
+                int cmd = TrackPopupMenu(hMenu, 0x100, (int)e.GetPosition(this).X, (int)e.GetPosition(this).Y, 0, hWnd, IntPtr.Zero);
+                if (cmd > 0) SendMessage(hWnd, 0x112, (IntPtr)cmd, IntPtr.Zero);
+            }
+        }
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+        [DllImport("user32.dll")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32.dll")]
+        static extern int TrackPopupMenu(IntPtr hMenu, uint uFlags, int x, int y,
+           int nReserved, IntPtr hWnd, IntPtr prcRect);
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT rect);
+        struct RECT { public int left, top, right, bottom; }
+
+        private void Window_LeftMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }   
     }
 }
